bfe67408ecb6ad2333bbd0e22783fa30fbc697fd
andrew.yatsenko <1solo4ndrew88@gmail.com>
2020-02-07 23:45:33 +0200

    Initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,5 +7,18 @@ hamburger.addEventListener('click', () => {
     body.classList.toggle('active');
 });
 
+window.addEventListener('scroll', () => {
+    const sideNav = document.getElementById('up-button');
+
+    if(window.pageYOffset < 500){
+        sideNav.style.visibility = 'hidden';
+        sideNav.style.opacity = '0';
+
+    } else {
+        sideNav.style.visibility = 'visible';
+        sideNav.style.opacity = '1';
+    }
+})
+
 
 AOS.init();
